82a6f639ae4f0d78bbe05ecc11ecb66e6ee867f0
marco-gulino <marcogulino98@gmail.com>
2025-03-25 16:25:57 +0100

    [feat] add function store in OrderController that handles the creation of new orders

--- Controllers/OrderController.js
+++ Controllers/OrderController.js
@@ -2,6 +2,56 @@
 const connection = require("../config/data");
 
 // STORE
-function store(req, res) {}
+function store(req, res) {
+  // ORDER Infos
+  const {
+    name,
+    surname,
+    email,
+    shipment_address,
+    products,
+    city,
+    phone_num,
+    billing_address,
+    cf,
+    total_price,
+    id_coupon,
+  } = req.body;
+
+  if (!products || products.length === 0) {
+    return res.status(400).json({ error: "No Products Selected" });
+  }
+
+  console.log(req.body);
+  // QUERY
+  const addOrder = `INSERT INTO orders (name, surname, email, shipment_address, city, phone_num, billing_address, cf)
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+
+  // Inject QUERY
+  connection.query(
+    addOrder,
+    [
+      name,
+      surname,
+      email,
+      shipment_address,
+      city,
+      phone_num,
+      billing_address,
+      cf,
+    ],
+    (err, resultOrder) => {
+      // Query Failed
+      if (err)
+        return res
+          .status(500)
+          .json({ error: "Database query failed", details: err.message });
+
+      res.status(201).json(resultOrder[0]);
+      //Add Products to Order
+      //const orderProducts = products.map(product => [Id, product.id, product.quantity]);
+    }
+  );
+}
 
 module.exports = { store };
