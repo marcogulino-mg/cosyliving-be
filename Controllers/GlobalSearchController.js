3f220f89d7883a533df1965edd4e95bdae08e928
marco-gulino <marcogulino98@gmail.com>
2025-03-26 15:30:46 +0100

    [feat] change from slug to name in showProducts query

--- Controllers/GlobalSearchController.js
+++ Controllers/GlobalSearchController.js
@@ -1,26 +1,25 @@
 const connection = require("../config/data");
 
-
 function search(req, res) {
-    // Slug from REQ BODY
-    const { slug } = req.params;
+  // Slug from REQ BODY
+  const { slug } = req.params;
 
-    if (!slug) return res.status(400).json({ error: "Missing Param" });
+  if (!slug) return res.status(400).json({ error: "Missing Param" });
 
-    // QUERY
-    const showProducts = `SELECT * FROM products WHERE slug = ?`;
+  // QUERY
+  const showProducts = `SELECT * FROM products WHERE name = ?`;
 
-    // Inject Query
-    connection.query(showProducts, [slug], (err, prodResult) => {
-        // Query Failed
-        if (err) return res.status(500).json({ error: "Database query failed" });
-        // Query Empty
-        if (prodResult.length === 0)
-            return res.status(404).json({ error: "Missing Product" });
+  // Inject Query
+  connection.query(showProducts, [slug], (err, prodResult) => {
+    // Query Failed
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    // Query Empty
+    if (prodResult.length === 0)
+      return res.status(404).json({ error: "Missing Product" });
 
-        // SEND RES
-        res.json(prodResult);
-    });
+    // SEND RES
+    res.json(prodResult);
+  });
 }
 
-module.exports = { search }
+module.exports = { search };
